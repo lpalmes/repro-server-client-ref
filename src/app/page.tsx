7478f1f4638db0a79cde851b7c3cef0941eca974
Lorenzo Palmes <lpalmes@gmail.com>
2024-11-06 13:20:27 +0000

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-import { fetchGraphql, graphql } from "@/lib/graphql";
-import { Pokemon, PokemonFragment } from "./pokemon";
-
-export const dynamic = "force-dynamic";
+import { PokemonConfig } from "./pokemon";
 
 export default async function Home() {
-  const data = await fetchGraphql(
-    graphql(
-      `
-        query pageQuery {
-          getPokemon(pokemon: dragonite) {
-            ...PokemonFragment
-          }
-        }
-      `,
-      [PokemonFragment]
-    ),
-    {}
-  );
   return (
     <main>
-      <Pokemon pokemon={data.getPokemon} />
+      <h1>{PokemonConfig.data.name}</h1>
     </main>
   );
 }
--- src/app/pokemon.tsx
+++ src/app/pokemon.tsx
@@ -1,25 +1,7 @@
 "use client";
 
-import { FragmentOf, graphql, readFragment } from "@/lib/graphql";
-
-export const PokemonFragment = graphql(`
-  fragment PokemonFragment on Pokemon {
-    species
-    color
-  }
-`);
-
-type Props = {
-  pokemon: FragmentOf<typeof PokemonFragment>;
+export const PokemonConfig = {
+  data: {
+    name: "Test name",
+  },
 };
-
-export function Pokemon(props: Props) {
-  const pokemon = readFragment(PokemonFragment, props.pokemon);
-
-  return (
-    <div>
-      <h1>Species: {pokemon.species}</h1>
-      <p>Color: {pokemon.color}</p>
-    </div>
-  );
-}
